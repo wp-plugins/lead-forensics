b393b3d9c83be026c8c53812ce805d0bc00f0a28
douglaskarr <douglaskarr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-06 12:25:36 +0000

    Version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/lead-forensics/trunk@1175846 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lead-forensics.php
@@ -0,0 +1,69 @@
+<?php
+	/*
+	Plugin Name: Lead Forensics
+	Plugin URI: http://wordpress.org/extend/plugins/leadforensics/
+	Description: Easily implement your Lead Forensics tracking code in WordPress
+	Version: 1.0.1
+	Author: Douglas Karr
+	Author URI: http://www.dknewmedia.com
+
+	Copyright 2015 DK New Media  (email : info@dknewmedia.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as 
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	*/
+
+
+	// add the admin options page
+	add_action('admin_menu', 'plugin_admin_add_wplf_page');
+	add_action('admin_init', 'wplf_register_settings' );
+	add_action('wp_head', 'wplf_place_tracking_code');
+
+	function plugin_admin_add_wplf_page() {
+		add_options_page('Lead Forensics Tracking Code', 'Lead Forensics', 'manage_options', 'wplf_plugin', 'wplf_plugin_options_page');
+	}
+
+	function wplf_plugin_options_page() {
+		?>
+		<h2>Lead Forensics</h2>
+		<p><a href='https://www.marketingtechblog.com/b2b-identify-website-visitors/' target='_blank'>Lead Forensics</a> is a B2B tool used to identify the unidentified visitors that are arriving at your site.</p><p>This is a simple plugin to assist you in placing the <a href='https://portal.leadforensics.com/TrackingCode' target='blank'>Tracking Code</a> into your WordPress site or blog.</p>
+		
+		<form method="post" action="options.php">
+    	<?php settings_fields( 'wplf-settings-group' ); ?>
+    	<?php do_settings_sections( 'wplf-settings-group' ); ?>
+    	<table class="form-table">
+        	<tr valign="top">
+        	<th scope="row">Tracking Code</th>
+        	<td><textarea rows='3' columns='100' style='width:640px' name='wplf_code'><?php echo esc_textarea( get_option('wplf_code') ); ?></textarea></td>
+        	</tr>
+       </table>
+    	
+    	<?php submit_button(); ?>
+
+		</form>
+		<p><small>This plugin was developed by <a href="http://www.dknewmedia.com" target="_blank">DK New Media</a> at no cost to <a href="https://www.marketingtechblog.com/b2b-identify-website-visitors/">Lead Forensics</a>, but we've included a landing page where we are compensated for referrals.</small></p>
+		<h3>About Lead Forensics</h3>
+		<p><iframe width="640" height="360" src="https://www.youtube.com/embed/gP-Ol1AfBoQ?showinfo=0" frameborder="0" allowfullscreen></iframe></p>
+		</div>	
+		<?php
+	}
+	
+	function wplf_register_settings() {
+		register_setting( 'wplf-settings-group', 'wplf_code' );
+	}
+	
+	function wplf_place_tracking_code() {
+		echo get_option('wplf_code');
+	}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,52 @@
+=== Plugin Name ===
+Contributors: douglaskarr
+Donate link: http://www.dknewmedia.com
+Tags: leadforensics, lead-forensics, leads, tracking-code, b2b, anonymous visitors, sales, sales leads
+Requires at least: 3.0.1
+Tested up to: 4.1.5
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+[Lead Forensics](http://lead-generation.leadforensics.com/partnership-marketing-tech-blog/?utm_source=plugin-readme&utm_medium=description&utm_campaign=1) allows you to Turn your anonymous website visitors into sales leads, convert new business opportunities before your competitiors and increase your online ROI. This plugin allows you to easily add your tracking code from <a href=““ target=“_blank”>Lead Forensics</a> to the head of your WordPress site. 
+
+[youtube https://www.youtube.com/watch?v=gP-Ol1AfBoQ]
+
+Lead Forensics Features:
+* Identify your anonymous business visitors and access their business name, telephone number, address, industry and company size.
+* Close sales you never knew you had since 98% of businesses don’t make an inquiry
+* Take action on leads before your competitors, create watch lists and even assign leads my geography
+* Increase your online ROI by getting more leads out of your site
+* Improve relationships with your current clients by setting alerts as they visit the site and review additional product or service offerings.
+
+This plugin was developed by [DK New Media](http://www.dknewmedia.com?utm_source=lead-forensics-plugin&utm_medium=readme&utm_campaign=1)
+
+
+== Installation ==
+**Way 1:**<br>
+1. Download the plugin file<br>
+2. Unzip the file into a folder on your hard drive<br>
+3. Upload via FTP into your plugins (/wp-content/plugins/) folder<br>
+4. Activate the plugin from your dashboard<br>
+
+**Way 2:**<br>
+1. From Dashboard > Plugins > Add New > Upload the plugin<br>
+2. Activate the plugin<br>
+
+**How to use:**<br>
+1. Click on the Settings menu item - “Lead Forensics“<br>
+2. Add your data
+
+== Screenshots ==
+
+1. Panel for adding script and styles
+
+== Frequently Asked Questions ==
+= Is support available? =
+Yes
+
+== Changelog ==
+
+= 1.0 =
+Initial Release
